cfd3edaf6e66d519e0a9a7721435bd4413adc114
andreaGaudino <andrea.gaudino02@gmail.com>
2024-05-22 10:42:29 +0200

    fine punto 1

--- model/model.py
+++ model/model.py
@@ -17,6 +17,7 @@ class Model:
         self.colors = DAO.getAllColors()
         return self.colors
     def createGraph(self, color, year):
+        self.grafo.clear()
         for p in DAO.getProductsByColor(color):
             self.idProduct[p.Product_number] = p
 
@@ -45,7 +46,9 @@ class Model:
     def ricercaPercorso(self, product):
         self.solBest = []
         self.nodoPartenza = product
+        #print(self.grafo.edges)
         self.ricorsione(product, [], [])
+
 
 
     def ricorsione(self, product, parziale, listaNodi):
@@ -55,24 +58,32 @@ class Model:
         for arco in archi_uscenti:
             if len(parziale) == 0:
                 proseguo = True
-            elif arco[2]["weight"] >= parziale[-1][2]["weight"]:
+                if arcoPiuLungo is None or arco[2]["weight"] > arcoPiuLungo[2]["weight"]:
+                    arcoPiuLungo = arco
+            elif arco[2]["weight"] >= parziale[-1][2]["weight"] and arco[1] not in listaNodi:
                 proseguo = True
                 if arcoPiuLungo is None or arco[2]["weight"] > arcoPiuLungo[2]["weight"]:
                     arcoPiuLungo = arco
 
         #condizione terminale
         if not proseguo:
-            if len(parziale) > len(self.solBest) and :
+            if len(parziale) > len(self.solBest) and self.nodoPartenza in listaNodi:
                 self.solBest = parziale
 
+
         else:
+            parziale.append(arcoPiuLungo)
+            listaNodi.append(product)
+            print(len(parziale),parziale)
+            self.ricorsione(arcoPiuLungo[1], parziale, listaNodi)
+            parziale.pop()
+            listaNodi.pop()
 
 
 
 
-
-    def checkVincoli(self):
-        pass
+    # def checkVincoli(self):
+    #     pass
 
     def getNumNodes(self):
         return len(self.grafo.nodes)
